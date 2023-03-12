09e0a7a965aa9cdfa5460bdb29b6c5e2c2413139
kenta-crv <okuyamakenta@gmail.com>
2023-03-12 19:48:08 +0900

    fix

--- test/controllers/cultures_controller_test.rb
+++ test/controllers/cultures_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class CalturesControllerTest < ActionDispatch::IntegrationTest
+class CulturesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
